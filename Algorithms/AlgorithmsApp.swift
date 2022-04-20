c039c858a2f988d5cb04df6d08af71272d37fef5
Simon Robatto <simon.robatto-simard@polymtl.ca>
2022-04-20 17:20:15 -0400

    Initial Commit

---
+++ Algorithms/AlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsApp.swift
+//  Algorithms
+//
+//  Created by Simon Robatto on 2022-04-20.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
