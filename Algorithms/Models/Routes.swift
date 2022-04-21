e775b2441626630b297db3984839d736d587d753
Simon Robatto <simon.robatto-simard@polymtl.ca>
2022-04-21 12:20:26 -0400

    Forgot to include routes

---
+++ Algorithms/Models/Routes.swift
@@ -0,0 +1,14 @@
+//
+//  Routes.swift
+//  Algorithms
+//
+//  Created by Simon Robatto on 2022-04-20.
+//
+
+import Combine
+
+enum Routes {
+    case none
+    case quicksort, bubbleSort, insertionSort
+    case dijkstra, prim
+}
